9df5345f496fa80cd375d6847bc7ca36ce021d01
Catalin Tudorache <catalin.tudorache87@gmail.com>
2021-12-14 12:00:16 +0200

    Added register.

--- webshop-fork-ejs/routes/auth.js
+++ webshop-fork-ejs/routes/auth.js
@@ -1,12 +1,35 @@
 var express = require("express");
+var fs = require("fs");
 var router = express.Router();
 
 router.get("/login", function (req, res, next) {
-  res.render("login", { title: "Login", register: req.query.register });
+  res.render("login", { title: "Login", register: req.query.register, error: req.query.error });
+});
+
+router.post("/login", function (req, res, next) {
+
+  // read username and password
+  let username = req.body.username;
+  let password = req.body.password;
+
+  // get users from file
+  let users = fs.readFileSync('./data/users.json', { encoding: 'utf8' });
+
+  // check if credentials match any existing user
+  let loggedInUser = users.find(user => user.username === username && user.password === password);
+
+  if (loggedInUser) {
+    // create cookie with user session and redirect to homepage
+
+    res.redirect('/home');
+  } else {
+    // redirect to login and show error message
+    res.redirect('/auth/login?error=true');
+  }
+
 });
 
 router.get("/register", function (req, res, next) {
-
   res.render("register", { title: "Register" });
 });
 
